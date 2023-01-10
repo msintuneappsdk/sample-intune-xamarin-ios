985aefd2c5cee7705eac93530aef21005670388c
Kyle Reis <Kyle-Reis@users.noreply.github.com>
2023-01-10 18:06:16 -0500

    Address build warning for use of deprecated version of UIApplication.Main()

--- IntuneMAMSampleiOS/Main.cs
+++ IntuneMAMSampleiOS/Main.cs
@@ -15,7 +15,8 @@ namespace IntuneMAMSampleiOS
         {
             // if you want to use a different Application Delegate class from "AppDelegate"
             // you can specify it here.
-            UIApplication.Main (args, null, "AppDelegate");
+            UIApplication.Main (args, null, typeof(AppDelegate));
+
         }
     }
 }
