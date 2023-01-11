33e78c0d88e317d55b1f2e21f2e508f86ebf1b1e
Gustavo Jiménez <gustavoja@gmail.com>
2023-01-11 18:48:14 +0100

    Publish event on enrollment status changed

---
+++ IntuneMAMSampleiOS/EnrollmentEventArgs.cs
@@ -0,0 +1,15 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="MainControllerEnrollmentDelegate.cs" company="Microsoft">
+//     Copyright (c) Microsoft Corporation.  All rights reserved.
+// </copyright>
+//-----------------------------------------------------------------------
+
+using System;
+
+namespace IntuneMAMSampleiOS
+{
+    public class EnrollmentEventArgs : EventArgs
+    {
+        public bool Enrolled { get; set; }
+    }
+}
