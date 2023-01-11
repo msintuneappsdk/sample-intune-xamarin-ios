dddfaf9c217e7b2f88c4b7f97332b0cb8c237a93
Gustavo Jiménez <gustavoja@gmail.com>
2023-01-11 18:49:00 +0100

    Improve UI to be responsive and inform loading status

--- IntuneMAMSampleiOS/MainViewController.designer.cs
+++ IntuneMAMSampleiOS/MainViewController.designer.cs
@@ -1,121 +1,138 @@
 // WARNING
 //
-// This file has been generated automatically by Visual Studio from the outlets and
-// actions declared in your storyboard file.
-// Manual changes to this file will not be maintained.
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
 //
 using Foundation;
-using System;
 using System.CodeDom.Compiler;
-using UIKit;
 
 namespace IntuneMAMSampleiOS
 {
-    [Register ("MainViewController")]
-    partial class MainViewController
-    {
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton buttonLogIn { get; set; }
+	[Register ("MainViewController")]
+	partial class MainViewController
+	{
+		[Outlet]
+		UIKit.UIActivityIndicatorView activityIndicator { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton buttonLogOut { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton buttonLogIn { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton buttonSave { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton buttonLogOut { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton buttonShare { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton buttonSave { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UIButton buttonUrl { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton buttonShare { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UILabel labelEmail { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UIButton buttonUrl { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UITextField textCopy { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UILabel labelEmail { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UITextField textEmail { get; set; }
+		[Outlet]
+		UIKit.UILabel labelEnrollState { get; set; }
 
-        [Outlet]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        UIKit.UITextField textUrl { get; set; }
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UITextField textCopy { get; set; }
 
-        [Action ("ButtonLogIn_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void ButtonLogIn_TouchUpInside (UIKit.UIButton sender);
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UITextField textEmail { get; set; }
 
-        [Action ("ButtonLogOut_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void ButtonLogOut_TouchUpInside (UIKit.UIButton sender);
+		[Outlet]
+		[GeneratedCode ("iOS Designer", "1.0")]
+		UIKit.UITextField textUrl { get; set; }
 
-        [Action ("ButtonSave_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void ButtonSave_TouchUpInside (UIKit.UIButton sender);
+		[Outlet]
+		UIKit.UIView viewEnrollState { get; set; }
 
-        [Action ("buttonShare_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void buttonShare_TouchUpInside (UIKit.UIButton sender);
+		[Action ("ButtonLogIn_TouchUpInside:")]
+		partial void ButtonLogIn_TouchUpInside (UIKit.UIButton sender);
 
-        [Action ("buttonUrl_TouchUpInside:")]
-        [GeneratedCode ("iOS Designer", "1.0")]
-        partial void buttonUrl_TouchUpInside (UIKit.UIButton sender);
+		[Action ("ButtonLogOut_TouchUpInside:")]
+		partial void ButtonLogOut_TouchUpInside (UIKit.UIButton sender);
 
-        void ReleaseDesignerOutlets ()
-        {
-            if (buttonLogIn != null) {
-                buttonLogIn.Dispose ();
-                buttonLogIn = null;
-            }
+		[Action ("ButtonSave_TouchUpInside:")]
+		partial void ButtonSave_TouchUpInside (UIKit.UIButton sender);
 
-            if (buttonLogOut != null) {
-                buttonLogOut.Dispose ();
-                buttonLogOut = null;
-            }
+		[Action ("buttonShare_TouchUpInside:")]
+		partial void buttonShare_TouchUpInside (UIKit.UIButton sender);
 
-            if (buttonSave != null) {
-                buttonSave.Dispose ();
-                buttonSave = null;
-            }
+		[Action ("buttonUrl_TouchUpInside:")]
+		partial void buttonUrl_TouchUpInside (UIKit.UIButton sender);
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (buttonLogIn != null) {
+				buttonLogIn.Dispose ();
+				buttonLogIn = null;
+			}
 
-            if (buttonShare != null) {
-                buttonShare.Dispose ();
-                buttonShare = null;
-            }
+			if (buttonLogOut != null) {
+				buttonLogOut.Dispose ();
+				buttonLogOut = null;
+			}
 
-            if (buttonUrl != null) {
-                buttonUrl.Dispose ();
-                buttonUrl = null;
-            }
+			if (buttonSave != null) {
+				buttonSave.Dispose ();
+				buttonSave = null;
+			}
 
-            if (labelEmail != null) {
-                labelEmail.Dispose ();
-                labelEmail = null;
-            }
+			if (buttonShare != null) {
+				buttonShare.Dispose ();
+				buttonShare = null;
+			}
 
-            if (textCopy != null) {
-                textCopy.Dispose ();
-                textCopy = null;
-            }
+			if (buttonUrl != null) {
+				buttonUrl.Dispose ();
+				buttonUrl = null;
+			}
 
-            if (textEmail != null) {
-                textEmail.Dispose ();
-                textEmail = null;
-            }
+			if (labelEmail != null) {
+				labelEmail.Dispose ();
+				labelEmail = null;
+			}
 
-            if (textUrl != null) {
-                textUrl.Dispose ();
-                textUrl = null;
-            }
-        }
-    }
+			if (textCopy != null) {
+				textCopy.Dispose ();
+				textCopy = null;
+			}
+
+			if (textEmail != null) {
+				textEmail.Dispose ();
+				textEmail = null;
+			}
+
+			if (textUrl != null) {
+				textUrl.Dispose ();
+				textUrl = null;
+			}
+
+			if (labelEnrollState != null) {
+				labelEnrollState.Dispose ();
+				labelEnrollState = null;
+			}
+
+			if (viewEnrollState != null) {
+				viewEnrollState.Dispose ();
+				viewEnrollState = null;
+			}
+
+			if (activityIndicator != null) {
+				activityIndicator.Dispose ();
+				activityIndicator = null;
+			}
+		}
+	}
 }
